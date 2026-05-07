b63fc698cde68a2e2fbabfece377aeccb2e83a11
SPOTWEBDEV <spotwebdev.com@gmail.com>
2026-05-07 10:26:45 +0100

    work on the auth

--- server/auth/client.php
+++ server/auth/client.php
@@ -9,6 +9,12 @@ $select = mysqli_query($connection, "SELECT * FROM users WHERE id = $user_id");
 $client = mysqli_fetch_assoc($select);
 
 
+if(mysqli_num_rows($select) == 0 ) {
+    session_destroy();
+    header("location: {$domain}auth/sign_in/");
+}
+
+
 if($client['status'] == 'suspended') {
     session_destroy();
     header("location: {$domain}auth/sign_in/");
